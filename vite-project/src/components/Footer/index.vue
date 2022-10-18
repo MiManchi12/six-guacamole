900df62b8584d5f84dc1f18ca6d0fdc7afb6317b
mi-manchi12 <1255199404@qq.com>
2022-10-18 17:47:03 +0800

    12

---
+++ vite-project/src/components/Footer/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        尾部
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Vue from 'vue'
+// 定义全局组件
+import Header from './components/Header/index.vue'
+import Footer from './components/Footer/index.vue'
+//全局组件:组件名字   第二个参数：组件
+Vue.component(Header.name,Header);
+Vue.component(Footer.name,Footer);
+
 
 createApp(App).mount('#app')
